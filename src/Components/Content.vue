dc4b11c189fa60b6d8be912bcbf27a819f99a096
Cihan <chnbzkrt30@gmail.com>
2020-02-04 16:14:40 +0300

    blog

--- src/Components/Content.vue
+++ src/Components/Content.vue
@@ -1,80 +1,17 @@
 <template>
   <section>
-    <div class="cardb clearfix">
-      <div class="contentimg"><img src="https://i.picsum.photos/id/1/880/660.jpg" width="300" height="250"
-          alt="Blog İmage"></div>
-      <div class="contenttext">
-        <div class="blog-meta"><time class="blogtime">January 24,
-            2016</time><span>/ </span><a class="authorname">Thomas</a></div><a>
-          <h3>Magazine WordPress Theme</h3>
-        </a>
-        <p>Lorem ipsum dolor sit amet,
-          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        </p><a class="continuiereading">Continue reading <i class="fas fa-angle-double-right"></i></a>
-      </div>
-    </div>
-    <div class="cardb clearfix">
-      <div class="contentimg"><img src="https://i.picsum.photos/id/42/880/660.jpg" width="300" height="250"
-          alt="Blog İmage"></div>
-      <div class="contenttext">
-        <div class="blog-meta"><time class="blogtime">January 24,
-            2016</time><span>/ </span><a class="authorname">Thomas</a></div><a>
-          <h3>Magazine WordPress Theme</h3>
-        </a>
-        <p>Lorem ipsum dolor sit amet,
-          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        </p><a class="continuiereading">Continue reading <i class="fas fa-angle-double-right"></i></a>
-      </div>
-    </div>
-    <div class="cardb clearfix">
-      <div class="contentimg"><img src="https://i.picsum.photos/id/33/880/660.jpg" width="300" height="250"
-          alt="Blog İmage"></div>
-      <div class="contenttext">
-        <div class="blog-meta"><time class="blogtime">January 24,
-            2016</time><span>/ </span><a class="authorname">Thomas</a></div><a>
-          <h3>Magazine WordPress Theme</h3>
-        </a>
-        <p>Lorem ipsum dolor sit amet,
-          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        </p><a class="continuiereading">Continue reading <i class="fas fa-angle-double-right"></i></a>
-      </div>
-    </div>
-    <div class="cardb clearfix">
-      <div class="contentimg"><img src="https://i.picsum.photos/id/24/880/660.jpg" width="300" height="250"
-          alt="Blog İmage"></div>
-      <div class="contenttext">
-        <div class="blog-meta"><time class="blogtime">January 24,
-            2016</time><span>/ </span><a class="authorname">Thomas</a></div><a>
-          <h3>Magazine WordPress Theme</h3>
-        </a>
-        <p>Lorem ipsum dolor sit amet,
-          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        </p><a class="continuiereading">Continue reading <i class="fas fa-angle-double-right"></i></a>
-      </div>
-    </div>
-    <div class="cardb clearfix">    
-      <div class="contentimg"><img src="https://i.picsum.photos/id/15/880/660.jpg" width="300" height="250"
-          alt="Blog İmage"></div>
-      <div class="contenttext">
-        <div class="blog-meta"><time class="blogtime">January 24,
-            2016</time><span>/ </span><a class="authorname">Thomas</a></div><a>
-          <h3>Magazine WordPress Theme</h3>
-        </a>
-        <p>Lorem ipsum dolor sit amet,
-          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        </p><a class="continuiereading">Continue reading <i class="fas fa-angle-double-right"></i></a>
-      </div>
-    </div>
-
-
-    <div class="olderposts text-center"><button>Older Posts</button></div>
-   
-
+    <Blogs></Blogs>
   </section>
 </template>
 
 <script>
+import Blogs from './ContentComponents/Blogs'
+import Blog from './ContentComponents/Blog'
 export default {
+  components:{
+    Blogs,
+    Blog
+  },
 
   name: 'Content',
   data() {
